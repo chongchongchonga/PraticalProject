181d7d823fa030b5fab23e5fc2351a0018e8c004
chongchongchonga <82813731+chongchongchonga@users.noreply.github.com>
2021-04-27 22:58:03 +0800

    sql

---
+++ library.sql
@@ -0,0 +1,10 @@
+INSERT INTO `library` (`BookName`, `BookId`) VALUES ('Python3.x编程入门图书，机器学习，数据处理，网络爬虫热门编程语言，从基本概念到完整项目开发，帮助零基础读者迅速掌握Python编程，附赠源代码文件 100册以上团购优惠联系电话010-89111488\r\n', 1);
+INSERT INTO `library` (`BookName`, `BookId`) VALUES ('Python3.x编程入门图书，机器学习，数据处理，网络爬虫热门编程语言，从基本概念到完整项目开发，帮助零基础读者迅速掌握Python编程，附赠源代码文件 100册以上团购优惠联系电话010-89111488\r\n', 2);
+INSERT INTO `library` (`BookName`, `BookId`) VALUES ('Python编程畅销经典套装，Python入门零基础自学教程书籍，Python项目案例开发实战，一套书搞定Python编程\r\n', 6);
+INSERT INTO `library` (`BookName`, `BookId`) VALUES ('Python编程畅销经典套装，Python入门零基础自学教程书籍，Python项目案例开发实战，一套书搞定Python编程\r\n', 7);
+INSERT INTO `library` (`BookName`, `BookId`) VALUES ('web前端开发经典教程，JS“红宝书”全新升级，入门+实战，涵盖ECMAScript2019，提供教学视频+配套编程环境，可直接在线运行随书代码 100册以上团购优惠联系电话010-89111488\r\n', 9);
+INSERT INTO `library` (`BookName`, `BookId`) VALUES ('web前端开发经典教程，JS“红宝书”全新升级，入门+实战，涵盖ECMAScript2019，提供教学视频+配套编程环境，可直接在线运行随书代码 100册以上团购优惠联系电话010-89111488\r\n', 10);
+INSERT INTO `library` (`BookName`, `BookId`) VALUES ('人民邮电出版社\r\n', 3);
+INSERT INTO `library` (`BookName`, `BookId`) VALUES ('人民邮电出版社\r\n', 8);
+INSERT INTO `library` (`BookName`, `BookId`) VALUES ('精通Python编程两件套：Python编程从入门到实践+Python数据结构与算法分析 第2版（京东套装共2册）\r\n', 4);
+INSERT INTO `library` (`BookName`, `BookId`) VALUES ('精通Python编程两件套：Python编程从入门到实践+Python数据结构与算法分析 第2版（京东套装共2册）\r\n', 5);
